2c1152e81894f51e9fcbb9a30b739e2f63c60808
nkinnas <nkinnas@trinity.edu>
2023-09-18 17:41:59 -0500

    commit 1

--- Scavenge.hs
+++ Scavenge.hs
@@ -21,7 +21,40 @@ type Dictionary = [String]
 -- system: https://hasbro-new.custhelp.com/app/answers/detail/a_id/55/
 -- A helper function may be useful.
 score :: Move -> Integer
-score x = undefined 
+score [] = 0
+sscore (x:xs) = case x of
+    'A' -> 1
+    'E' -> 1
+    'I' -> 1
+    'O' -> 1
+    'U' -> 1
+    'L' -> 1
+    'N' -> 1
+    'S' -> 1
+    'T' -> 1
+    'R' -> 1
+    'D' -> 2
+    'G' -> 2
+    'B' -> 3
+    'C' -> 3
+    'M' -> 3
+    'P' -> 3
+    'F' -> 4
+    'H' -> 4
+    'V' -> 4
+    'W' -> 4
+    'Y' -> 4
+    'K' -> 5
+    'J' -> 8
+    'X' -> 8
+    'Q' -> 10
+    'Z' -> 10
+    _   -> error "Blank letter not valid"
+  + score xs
+
+
+
+
 -- score "QA" == 11
 -- score "JF" == 12
 
